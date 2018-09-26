effcd190d2f62ed692a8440733d11ad5ad0f6b5d
Marzena Starościk <mstaroscik@groupon.com>
2018-09-26 20:06:12 +0200

    zadanie z warsztatow + zadanie domowe

--- week1/day2.py
+++ week1/day2.py
@@ -1,1 +1,13 @@
-print(2)
+# Czy liczba jest podzielna przez 2
+"""
+Pamiętaj input zawsze zwraca string :P
+wczytac dane od usera
+"""
+number = input("Proszę podaj liczbę: ")
+# uzuskac wynik reszty z dzielenia
+result = int(number) % 2
+# porownac reszte z dzielenia i dopisac komentarz
+if result == 0:
+    print("liczba podzielna przez 2")
+else:
+    print("Liczba niepodzielna przez 2")
---
+++ week1/pracadomowa.py
@@ -0,0 +1,5 @@
+# output:
+# 'Arek woli psy, a najbardziej boksery'
+
+#nie wolno dostarczyc znakow z zewnatrz
+# można dowolnie przekształcać
