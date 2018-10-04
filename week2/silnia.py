6039da99b72f2a1cf8c400b17f781a3dfeef8813
Marzena Starościk <mstaroscik@groupon.com>
2018-10-04 13:23:38 +0200

    warsztaty 3.10 - zadanie silnia done

--- week2/silnia.py
+++ week2/silnia.py
@@ -1,3 +1,16 @@
 # czy mamy zakres?
 # pętla for
 # obliczyc silnie do 10 ...
+
+
+liczba = input("Podaj liczbę:")
+
+silnia = 1
+
+for number in range(int(liczba)):
+    number +=1
+    silnia = silnia*number
+    print(number)
+
+print(silnia)
+
