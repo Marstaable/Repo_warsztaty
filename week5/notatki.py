1ace1b0d9e334a51956ef73062c7a9f742fc5847
Marzena Starościk <mstaroscik@groupon.com>
2018-10-24 19:02:26 +0200

    week5

---
+++ week5/notatki.py
@@ -0,0 +1,15 @@
+# d= {"a":1}
+#
+#
+# del d["a"]
+#
+# print(d)
+
+#objekt - klasa nadrzędna/rodzic - zawsze to wpisujemy bo tak :P dorzuca pewne ukryte własciwoasci
+
+class Samochod(object):
+    pass
+    def __init__(self):
+        pass
+    # definicje danych
+    # definicje metod
